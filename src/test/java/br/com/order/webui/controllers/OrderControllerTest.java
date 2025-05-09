26e5107362ec3e602284cb61e637101c59f5fb80
Filipe de Deus <filipedeus2015@gmail.com>
2025-05-08 21:23:00 -0300

    fixing unit test

--- src/test/java/br/com/order/webui/controllers/OrderControllerTest.java
+++ src/test/java/br/com/order/webui/controllers/OrderControllerTest.java
@@ -30,7 +30,6 @@ import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -88,7 +87,7 @@ class OrderControllerTest {
 
     when(orderUseCase.updateOrderStatus(orderId)).thenReturn(getOrderResponse());
 
-    mockMvc.perform(patch("/api/v1/order/status/" + orderId))
+    mockMvc.perform(post("/api/v1/order/status/" + orderId))
       .andExpect(status().isOk())
       .andExpect(jsonPath("$.status", is("RECEIVED")));
   }
